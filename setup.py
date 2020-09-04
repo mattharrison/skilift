fb050f49035ec5d3bccd4360025000afc26d9c05
Matt Harrison <matthewharrison@gmail.com>
2020-09-04 16:39:22 -0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='skilift',
+      version='0.1.0',
+      description='Simulate skilift',
+      url='http://github.com/mattharrison/skilift',
+      author='Matt Harrison',
+      license='MIT',
+      py_modules=['skilift'],
+      zip_safe=False)
